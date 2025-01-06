dc68884368151c972338e191f341f2836d562c4c
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2025-01-06 00:20:44 -0500

    fixed compilation bug

--- components/ProjectCard.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export function ProjectCard({ project }: { project: Project }) {
-  return (
-    <Link href={`/experiences/${project.id}`}>
-      {/* rest of your card content */}
-    </Link>
-  );
-} 
