ca19f7ba7091759de0c6827447346eabe890e130
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2025-06-14 00:58:31 -0400

    update primary colours

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ const HomePage: React.FC = () => {
     <>
       <PageWrapper hasNavbar>
         <div className="flex flex-col h-[calc(100dvh-80px)]">
-          <h2 className="text-5xl text-primary">This is</h2>
+          <h2 className="text-5xl text-primary-600">This is</h2>
           <div className="flex flex-row gap-4">
-            <h1 className="text-[10rem] font-bold text-primary">Jesse</h1>
+            <h1 className="text-[10rem] font-bold text-primary-600">Jesse</h1>
             <h2 className="text-secondary">Heading 2</h2>
             <h3 className="text-tertiary">Heading 3</h3>
             <h4 className="text-forth">Heading 4</h4>
