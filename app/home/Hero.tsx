9bfa7434d03f5938e4b67e01a527c09928cd7b08
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2025-06-15 01:57:51 -0400

    feat: new hero

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -2,16 +2,19 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center h-[calc(100dvh)] bg-slate-600">
-      <h2 className="text-5xl text-colors-primary-600">This is</h2>
+    <div className="flex flex-col justify-center h-[calc(100dvh)]">
+      <h2 className="text-[4rem] font-semibold text-colors-primary-600">This is</h2>
       <div className="flex flex-row gap-4">
-        <h1 className="text-[10rem] font-bold text-colors-primary-600">
+        <h1 className="text-[8rem] font-bold text-colors-primary-600">
           {"<"}Jesse
+          {/* Jesse */}
         </h1>
-        <h1 className="text-[10rem] font-bold text-transparent [-webkit-text-stroke-width:6px] [-webkit-text-stroke-color:#566E52]">
+        <h1 className="text-[8rem] font-bold text-transparent [-webkit-text-stroke-width:6px] [-webkit-text-stroke-color:#566E52]">
           Huang{"/>"}
+          {/* Huang */}
         </h1>
       </div>
+        <h2 className="text-[4rem] font-semibold text-colors-primary-600 self-end">UW CS'27</h2>
     </div>
   );
 };
